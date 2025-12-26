8dfd893b42be6087ad0457521268dfe14ab34d11
palmoni5 <d8401948@gmail.com>
2025-12-26 15:31:28 +0200

    Refactor tooltip loading and event handling
    Refactor tooltip functionality to use jQuery's require for better modularity and maintainability.

--- script.js
+++ script.js
@@ -1,65 +1,83 @@
-$(window).on('action:ajaxify.end', function() {
-    if ($('#topic-preview-tooltip').length === 0) {
-        $('body').append('<div id="topic-preview-tooltip"><div class="preview-content">טוען...</div></div>');
-    }
-
-    var $tooltip = $('#topic-preview-tooltip');
-    var topicCache = {};
-    var leaveTimeout;
-
-    function scheduleHide() {
-        leaveTimeout = setTimeout(function() {
-            $tooltip.hide();
-        }, 50);
-    }
-
-    function cancelHide() {
-        if (leaveTimeout) {
-            clearTimeout(leaveTimeout);
-            leaveTimeout = null;
+require(['jquery'], function($) {
+    $(window).on('action:ajaxify.end', function() {
+        // בדיקה שהאלמנט לא קיים כבר כדי למנוע כפילויות
+        if ($('#topic-preview-tooltip').length === 0) {
+            $('body').append('<div id="topic-preview-tooltip"><div class="preview-content">טוען...</div></div>');
         }
-    }
 
-    $('[component="topic/header"] a, .topic-title a').on('mouseenter', function() {
-        cancelHide();
+        var $tooltip = $('#topic-preview-tooltip');
+        var topicCache = {};
+        var leaveTimeout;
 
-        var $link = $(this);
-        var href = $link.attr('href');
-        if (!href || !href.includes('/topic/')) return;
+        function scheduleHide() {
+            leaveTimeout = setTimeout(function() {
+                $tooltip.hide();
+            }, 50);
+        }
 
-        var linkOffset = $link.offset();
-        var linkHeight = $link.outerHeight();
-        var linkWidth = $link.outerWidth();
-        
-        var top = linkOffset.top + linkHeight + 5;
-        var left = linkOffset.left - (550 - linkWidth);
-        if (left < 10) left = linkOffset.left;
+        function cancelHide() {
+            if (leaveTimeout) {
+                clearTimeout(leaveTimeout);
+                leaveTimeout = null;
+            }
+        }
 
-        $tooltip.css({ top: top + 'px', left: left + 'px' }).show();
+        // שימוש ב-Delegate event כדי לתפוס גם אלמנטים שנוספו דינמית
+        $('body').on('mouseenter', '[component="topic/header"] a, .topic-title a', function() {
+            cancelHide();
 
-        if (topicCache[href]) {
-            $tooltip.find('.preview-content').html(topicCache[href]);
-        } else {
-            $tooltip.find('.preview-content').text('טוען...');
-            $.getJSON('/api' + href, function(data) {
-                if (data && data.posts && data.posts.length > 0) {
-                    var content = data.posts[0].content;
-                    topicCache[href] = content;
-                    if ($tooltip.is(':visible')) {
-                        $tooltip.find('.preview-content').html(content);
+            var $link = $(this);
+            var href = $link.attr('href');
+            
+            // בדיקת תקינות הקישור
+            if (!href || !href.includes('/topic/')) return;
+
+            var linkOffset = $link.offset();
+            var linkHeight = $link.outerHeight();
+            var linkWidth = $link.outerWidth();
+            
+            // חישוב מיקום ה-Tooltip
+            var top = linkOffset.top + linkHeight + 5;
+            var left = linkOffset.left - (550 - linkWidth); // 550 הוא רוחב ה-Tooltip
+            if (left < 10) left = linkOffset.left;
+
+            $tooltip.css({ top: top + 'px', left: left + 'px' }).show();
+
+            // טעינת התוכן
+            if (topicCache[href]) {
+                $tooltip.find('.preview-content').html(topicCache[href]);
+            } else {
+                $tooltip.find('.preview-content').text('טוען...');
+                
+                // שימוש ב-NodeBB API
+                $.getJSON('/api' + href, function(data) {
+                    if (data && data.posts && data.posts.length > 0) {
+                        var content = data.posts[0].content;
+                        topicCache[href] = content;
+                        
+                        // עדכון רק אם ה-Tooltip עדיין מוצג והמשתמש עדיין על אותו קישור
+                        if ($tooltip.is(':visible')) {
+                            $tooltip.find('.preview-content').html(content);
+                        }
                     }
-                }
-            });
-        }
-    }).on('mouseleave', function() {
-        scheduleHide();
-    });
+                }).fail(function() {
+                     $tooltip.find('.preview-content').text('שגיאה בטעינת התוכן');
+                });
+            }
+        });
 
-    $tooltip.on('mouseenter', function() {
-        cancelHide();
-    });
+        // הסתרה ביציאה מהקישור
+        $('body').on('mouseleave', '[component="topic/header"] a, .topic-title a', function() {
+            scheduleHide();
+        });
 
-    $tooltip.on('mouseleave', function() {
-        scheduleHide();
+        // טיפול בהובר על ה-Tooltip עצמו
+        $tooltip.on('mouseenter', function() {
+            cancelHide();
+        });
+
+        $tooltip.on('mouseleave', function() {
+            scheduleHide();
+        });
     });
 });
